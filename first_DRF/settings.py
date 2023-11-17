5c2f13d6d8e98a160b154e172280705a03efe3d7
SheikhRokon <mdrokon.u545@gmail.com>
2023-11-17 10:45:46 +0600

    model create

--- first_DRF/settings.py
+++ first_DRF/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'new_app',
 ]
 
 MIDDLEWARE = [
--- new_app/models.py
+++ new_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class StudentData(models.Model):
+    name = models.CharField(max_length=300)
+    student_id = models.CharField(max_length=20)
+    add = models.CharField(max_length=500)
+    class Meta:
+        verbose_name_plural ='Student Datas'
+
+    def __str__(self):
+        return self.name
+
