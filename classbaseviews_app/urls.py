2c5ee1cbc16f01775e5314c22343a2c2bbdad7d2
SheikhRokon <mdrokon.u545@gmail.com>
2023-11-26 12:30:33 +0600

    up date

--- classbaseviews_app/urls.py
+++ classbaseviews_app/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('snippets/',views.SnippetList.as_view(),name='snippet-list'),
     path('snippets/<int:pk>/',views.SnippetDetail.as_view(),name='snippet-detail'),
     path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
